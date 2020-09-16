85e079cc8848083023996e3a13288d38e7310e35
OsherK <osherzasx@gmail.com>
2020-09-16 16:34:50 +0300

    board bug fixes

--- src/store/reducers/boardReducer.js
+++ src/store/reducers/boardReducer.js
@@ -1,5 +1,5 @@
 const initialState={
-    board:{}
+    boards:[]
 }
 
 export function boardReducer(state=initialState, action){
@@ -8,6 +8,45 @@ export function boardReducer(state=initialState, action){
             return {
                 ...state,
                 board: action.board
+            }
+        case 'ADD_GROUP':
+            const boards = [...state.boards]
+            const boardIdx = boards.find(board => board._id === action.boardId);
+            boards[boardIdx].push({
+                _id: 124,
+                name: 'week1',
+                createdAt: 'date',
+                color: 'blue',
+                lastUpdated: 198465168486,
+                isTagsShown: false,
+                tags: [],
+                columns: [{
+                    type: 'date',
+                    name: 'due date'
+                }],
+                tasks: [{
+                    id: 21412,
+                    name: 'sneeze',
+                    createdAt: 1123124124241,
+                    members: [{
+                        _id: 1234,
+                        name: 'osher',
+                        imgUrl: 'www/sfasf'
+                    }],
+                    status: 'done/progress/stuck',
+                    priority: 1,
+                    dueDate: 214124124125,
+                    note: 'dont forget about this',
+                    lastUpdated: 'yesterday',
+                    isSelected: false,
+                    posts: [],
+                    tags: ['ui', 'ux'],
+                    attachedImgs: []
+                }]
+            })
+            return{
+                ...state,
+                boards
             }
         default:
             return state
