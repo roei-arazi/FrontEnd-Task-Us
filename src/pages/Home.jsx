60b20ed7db0cf26b62d1fcbc154cf0af21c7783c
Your Name <you@example.com>
2020-09-16 17:41:45 +0300

    more scscc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 class _Home extends Component {
 state = {
@@ -7,13 +8,24 @@ state = {
 }
 
 componentDidMount() {
-console.log(Home, this.props);
+
 }
 
 render() {
 return (
 <section className="home">
 <h1>Home</h1>
+<NavLink to="/board/123">
+<button>Start Here!</button>
+</NavLink>
+<NavLink to="/login">
+<button>Login</button>   
+</NavLink>
+<NavLink to="/signup">
+<button>Sign-up</button>
+</NavLink>
+
+
 </section>
 )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 class _Login extends Component {
 state = {
@@ -13,7 +14,12 @@ console.log(Login, this.props);
 render() {
 return (
 <section className="login">
-
+<div style={{marginLeft:'350px'}}>
+<h1>Under Construction...</h1>
+<NavLink to="/board/123">
+<button>Go To Board!</button>
+</NavLink>
+</div>
 </section>
 )
 }
