fb8631b55a06fb5ecd5bdae1d7043e55a97670fd
liamzety <58632095+liamzety@users.noreply.github.com>
2020-09-17 15:58:06 +0300

    user profile layout

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -4,30 +4,51 @@ import { Boardbar } from '../cmps/Boardbar';
 import { Navbar } from '../cmps/Navbar';
 
 class _UserProfile extends Component {
-state = {
+    state = {
 
-}
+    }
 
-componentDidMount() {
-}
+    componentDidMount() {
+    }
 
-render() {
-return (
-<section className="user-profile">
-<Navbar />
-<Boardbar />
-<div style={{marginLeft:'350px'}}>
-<h1>Under Construction...</h1>
-</div>
-</section>
-)
-}
+    render() {
+        return (
+            <section className="user-profile">
+                <Navbar />
+                <Boardbar />
+                <div className="user-container">
+                    <header className="header-container padding-x-15 flex justify-center  align-center">
+                        <img src="https://via.placeholder.com/250" alt="" />
+                    </header>
+
+                    {/* <form className="form-container flex justify-center column  align-center" action="">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" name="email" id="email" />
+                        <label htmlFor="fullName">Full name:</label>
+                        <input type="text" name="fullName" id="fullName" />
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" name="username" id="username" />
+                    </form> */}
+
+                    <div className="user-details-container padding-x-30 padding-y-30 align-center  flex column">
+                        <h2 className="clickable-header">Edit Profile</h2>
+                        <div className="user-details-inner-container">
+
+                            <h3>Email: user@email</h3>
+                            <h3>Full Name: Yitzhak yaka</h3>
+                            <h3>Username: yithak133</h3>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
 
 const mapStateToProps = state => {
-return {
+    return {
 
-}
+    }
 }
 
 const mapDispatchToProps = {
