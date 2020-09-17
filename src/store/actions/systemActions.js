b639b6dee890ee954c2d4d4c08bd48b9179f4bc5
liamzety <58632095+liamzety@users.noreply.github.com>
2020-09-17 18:19:00 +0300

    user edit modal

--- src/store/actions/systemActions.js
+++ src/store/actions/systemActions.js
@@ -1,10 +1,21 @@
 export function loading() {
-    return dispatch => {
-        dispatch({ type: 'LOADING_START' })
-    }
+  return dispatch => {
+    dispatch({ type: 'LOADING_START' })
   }
-  export function doneLoading() {
-    return dispatch=>{
-        dispatch({ type: 'LOADING_DONE' })
-    }
+}
+export function doneLoading() {
+  return dispatch => {
+    dispatch({ type: 'LOADING_DONE' })
   }
+}
+
+export function openModal() {
+  return dispatch => {
+    dispatch({ type: 'OPEN_MODAL' })
+  }
+}
+export function closeModal() {
+  return dispatch => {
+    dispatch({ type: 'CLOSE_MODAL' })
+  }
+}
--- src/store/reducers/systemReducer.js
+++ src/store/reducers/systemReducer.js
@@ -1,13 +1,21 @@
 const initialState = {
-    isLoading: false
-  };
-  
-  export function systemReducer(state = initialState, action = {}) {
-    switch (action.type) {
-      case 'LOADING_START':
-        return { ...state, isLoading: true };
-      case 'LOADING_DONE':
-        return { ...state, isLoading: false };
-      default: return state;
-    }
+  isLoading: false,
+  modal: {
+    isOpen: false,
   }
+}
+
+export function systemReducer(state = initialState, action = {}) {
+  switch (action.type) {
+    case 'LOADING_START':
+      return { ...state, isLoading: true };
+    case 'LOADING_DONE':
+      return { ...state, isLoading: false };
+    case 'OPEN_MODAL':
+      console.log('OPEN MODAL',)
+      return { ...state, modal: { ...state.modal, isOpen: true } };
+    case 'CLOSE_MODAL':
+      return { ...state, modal: { ...state.modal, isOpen: false } };
+    default: return state;
+  }
+}
