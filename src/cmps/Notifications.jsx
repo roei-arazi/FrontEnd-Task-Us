4da63118f2377d82d9a9e4e893cf09126027b150
liamzety <58632095+liamzety@users.noreply.github.com>
2020-09-18 14:12:03 +0300

    m

--- src/cmps/Notifications.jsx
+++ src/cmps/Notifications.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
+//FOR NOW USING HARDCODED FROM USER SERVICE
+//TODO: 
+// change from this file to the global state
+import { userService } from "../services/userService";
 
-export function Notifications() {
+export class Notifications extends Component {
 
-    return(
-        <section className="notifications">
-            <h3>notifications</h3>
-        </section>
-    )
+    state = {
+        users: null
+    }
 
+    async componentDidMount() {
+        const users = await userService.loadUsers()
+        console.log('USERS', users)
+        this.setState({ users })
+    }
+
+    render() {
+        if (!this.state.users) return <h1>Loading...</h1>
+        return (
+            <section className="notifications">
+
+                <div className="user-img-container">
+                    <img src={this.state.users[0].imgUrl} alt="profile" />
+                </div>
+                {this.state.users[0].name} changed
+
+            </section>
+        )
+    }
 }
+
