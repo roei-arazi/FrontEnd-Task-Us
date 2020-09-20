0b58fdcb81217e91130d50722ba8f18b7d80e7cd
OsherK <osherzasx@gmail.com>
2020-09-20 17:11:47 +0300

    m

--- src/cmps/Filter.jsx
+++ src/cmps/Filter.jsx
@@ -48,7 +48,7 @@ function _Filter(props) {
                             className={filterBy.memberId === member._id ? 'remove-filter-btn' : ''}
                             key={member._id}
                             onClick={() => onSetFilter('memberId', member._id)}>{member.fullName}</button>)}
-                    </div>
+                    </div> 
                 </section>
                 <section className="task-priority-filter flex column align-center">
                     <h3>Priority</h3>
