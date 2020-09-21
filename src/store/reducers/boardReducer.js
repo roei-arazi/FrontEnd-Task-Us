86f500b9ea1acfb1a91ade1737dbaebca67f6263
Your Name <you@example.com>
2020-09-21 12:00:24 +0300

    m

--- src/store/reducers/boardReducer.js
+++ src/store/reducers/boardReducer.js
@@ -40,14 +40,3 @@ export function boardReducer(state = initialState, action) {
             return state
     }
 }
-
-function _makeid(length = 7) {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-
-    return text;
-}
