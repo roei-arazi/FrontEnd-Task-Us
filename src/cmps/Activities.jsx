4193041e9be206cbc27550d07ade765de84ebe15
liamzety <58632095+liamzety@users.noreply.github.com>
2020-09-21 07:46:00 +0300

    m

--- src/cmps/Activities.jsx
+++ src/cmps/Activities.jsx
@@ -5,6 +5,7 @@ import { Tooltip, Zoom } from '@material-ui/core';
 import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 import { AiOutlineClose } from 'react-icons/ai';
 
+
 export default class Activities extends Component {
 
     state = {
@@ -15,7 +16,6 @@ export default class Activities extends Component {
     componentDidMount() {
         this.setState({ filteredActivities: this.props.activityLog })
     }
-
 
     handleSearch = ({ target }) => {
         const filteredActivities = this.props.activityLog.filter((activitiy) => {
@@ -43,13 +43,15 @@ export default class Activities extends Component {
     render() {
         if (!this.state.filteredActivities) return <h1>Loading...</h1>
         const { isOrderReversed, filteredActivities } = this.state;
-
         return (
             <section className="activities flex column padding-y-15">
+
                 <header className="padding-x-15">
+
                     <AiOutlineClose onClick={this.props.onToggleActivities} />
                     <h1><span>{this.props.boardName}</span> Log</h1>
-                    <div className="filters-container space-between flex align-center">
+
+                    <div className='filters-container space-between flex align-center'>
                         <input onChange={this.handleSearch} type="text" placeholder="Search" />
                         {isOrderReversed ?
                             <Tooltip enterDelay={200} TransitionComponent={Zoom} title="Order by date" arrow>
@@ -60,6 +62,7 @@ export default class Activities extends Component {
                             </Tooltip>
                         }
                     </div>
+
                 </header>
 
 
@@ -69,7 +72,7 @@ export default class Activities extends Component {
                         return (
                             <div key={activity.id} className="activity padding-x-15 space-between flex align-center">
                                 <div className="activity-desc-container flex align-center">
-                                    <div className="user-img-container">
+                                    <div className="user-img-container flex align-center justify-center">
                                         <img src={activity.byUser.imgUrl} alt="" />
                                     </div>
                                     <h2>{activity.byUser.fullName}</h2>
