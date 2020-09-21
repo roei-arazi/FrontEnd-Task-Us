24cfaeec96485d91e44bdd17b0b393fc37c00d4c
liamzety <58632095+liamzety@users.noreply.github.com>
2020-09-21 10:28:28 +0300

    m

--- src/cmps/task-cmps/Tags.jsx
+++ src/cmps/task-cmps/Tags.jsx
@@ -3,6 +3,7 @@ import Truncate from 'react-truncate'
 
 
 export function Tags(props) {
+
     function getRandomColor() {
         var letters = '0123456789ABCDEF';
         var color = '#';
@@ -11,13 +12,14 @@ export function Tags(props) {
         }
         return color;
     }
+
     return (
         <div className="label-container relative">
             <div onClick={() => props.openModal('tags')} className="tags">
                 <div className="task-label-name">
                     {props.tags.map((tag, idx) => {
                         return (
-                            <p>
+                            <p key={idx}>
                                 <Truncate lines={1} ellipsis={"..."} width={100}>
                                     {idx === props.tags.length - 1 ? tag : tag + ","}
                                 </Truncate>
@@ -31,7 +33,7 @@ export function Tags(props) {
                         <section>
                             {props.tags.map((tag, idx) => {
                                 return (
-                                    <p style={{ color: tag.color }}> {idx === props.tags.length - 1 ? tag : tag + ","}</p>
+                                    <p key={idx} style={{ color: tag.color }}> {idx === props.tags.length - 1 ? tag : tag + ","}</p>
                                 )
                             })}
                         </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,45 +2,46 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-import {loadBoards} from '../store/actions/boardActions'
+import { loadBoards } from '../store/actions/boardActions'
 
 class _Home extends Component {
-state = {
+    state = {
 
-}
+    }
 
-componentDidMount() {
-this.props.loadBoards()
-}
+    componentDidMount() {
+        this.props.loadBoards()
+    }
 
-render() {
-return (
-<section className="home">
-<h1>Home</h1>
-<NavLink to={`/board/${this.props.boards[0]._id}`}>
-<button>Start Here!</button>
-</NavLink>
-<NavLink to="/login">
-<button>Login</button>   
-</NavLink>
-<NavLink to="/signup">
-<button>Sign-up</button>
-</NavLink>
+    render() {
+        if (!this.props.boards || this.props.boards.length === 0) return <h1>Loading...</h1>
+        return (
+            <section className="home">
+                <h1>Home</h1>
+                <NavLink to={`/board/${this.props.boards[0]._id}`}>
+                    <button>Start Here!</button>
+                </NavLink>
+                <NavLink to="/login">
+                    <button>Login</button>
+                </NavLink>
+                <NavLink to="/signup">
+                    <button>Sign-up</button>
+                </NavLink>
 
 
-</section>
-)
-}
+            </section>
+        )
+    }
 }
 
 const mapStateToProps = state => {
-return {
-boards: state.boardReducer.boards
-}
+    return {
+        boards: state.boardReducer.boards
+    }
 }
 
 const mapDispatchToProps = {
-loadBoards
+    loadBoards
 }
 
 export const Home = connect(mapStateToProps, mapDispatchToProps)(_Home);
