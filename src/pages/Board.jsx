f775c209fda74cdd0e1454a6d5e97ee7e405e4e6
OsherK <osherzasx@gmail.com>
2020-09-22 08:56:24 +0300

    m

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -65,10 +65,10 @@ class _Board extends Component {
 
         const filteredBoard = JSON.parse(JSON.stringify(board))
         if (filterBy.groupId) {
-            board.groups = board.groups.filter(group => group.id === filterBy.groupId)
+            filteredBoard.groups = filteredBoard.groups.filter(group => group.id === filterBy.groupId)
         }
         function filterTasks(cb) {
-            board.groups = board.groups.map(group => {
+            filteredBoard.groups = filteredBoard.groups.map(group => {
                 group.tasks = group.tasks.filter(cb)
                 return group;
             })
@@ -258,6 +258,7 @@ class _Board extends Component {
 
     render() {
         if (this.props.boards.length === 0) return <h1>Loading...</h1>
+        console.log('got boards:', this.props.boards.length);
         const board = this.props.boards.find(board => board._id === this.state.boardId)
         const { users, filterBy } = this.props;
         if (!board) return <h1>Loading..</h1>
