21d28d65f06c12969029ca6dbedea58bd21e86a0
OsherK <osherzasx@gmail.com>
2020-09-23 13:24:29 +0300

    m

--- src/cmps/Activities.jsx
+++ src/cmps/Activities.jsx
@@ -15,34 +15,9 @@ export default class Activities extends Component {
         searchVal: ''
     }
 
-    // componentDidMount() {
-    //     const activities = this.props.activityLog.filter(activity => activity.isRead)
-    //     const activitiesNotRead = this.props.activityLog.filter(activity => !activity.isRead)
-    //     this.setState({ activities, activitiesNotRead })
-    // }
-
-    getActivities(){
+    get activities(){
         const {activityLog} = this.props;
         return [activityLog.filter(activity => activity.isRead), activityLog.filter(activity => !activity.isRead)]
-    }
-
-    handleSearch = ({ target }) => {
-        // const activities = this.props.activityLog.filter((activity) => {
-        //     return activity.isRead && (
-        //         activity.description.toLowerCase().includes(target.value.toLocaleLowerCase())
-        //         ||
-        //         activity.byUser.fullName.toLowerCase().includes(target.value.toLocaleLowerCase())
-        //     )
-        // })
-
-        // const activitiesNotRead = this.props.activityLog.filter((activity) => {
-        //     return !activity.isRead && (
-        //         activity.description.toLowerCase().includes(target.value.toLocaleLowerCase())
-        //         ||
-        //         activity.byUser.fullName.toLowerCase().includes(target.value.toLocaleLowerCase())
-        //     )
-        // })
-        // this.setState({ activitiesNotRead, activities })
     }
 
     handleChange = ({target}) =>{
@@ -54,11 +29,13 @@ export default class Activities extends Component {
     }
 
     onSetFilter(key, value){
-        this.setState({filterBy: {...this.state.filterBy, [key]: value}})
+        const filterBy = {...this.state.filterBy}
+        if(filterBy[key] === value) value = '';
+        this.setState({filterBy: {...filterBy, [key]: value}})
     }
 
-    getActivityDates(notRead, read) {
-        const activities = [...notRead, ...read];
+    getActivityDates() {
+        const activities = this.props.activityLog;
         const dates = activities.reduce((acc, activity) => {
             const date = moment(activity.createdAt).format('DD MMM');
             acc[date] = '';
@@ -67,19 +44,44 @@ export default class Activities extends Component {
         return Object.keys(dates);
     }
 
+    getActivityMembers(){
+        const activities = this.props.activityLog;
+        const members = activities.reduce((acc, activity) =>{
+            acc[activity.byUser.fullName] = '';
+            return acc;
+        }, {})
+        return Object.keys(members)
+    }
+
+    getActivityGroups(){
+        const activities = this.props.activityLog;
+        const groups = activities.reduce((acc, activity) =>{
+            acc[activity.group.name] = ''
+        }, {})
+        return Object.keys(groups)
+    }
+
     applyFilter(activities){
         let res = [...activities];
-        const {filterBy} = this.state;
+        const {filterBy, searchVal} = this.state;
         if(filterBy.date) res = res.filter(activity => moment(activity.createdAt).format('DD MMM') === filterBy.date);
+        if(filterBy.member) res = res.filter(activity => activity.byUser.fullName === filterBy.member)
+        if(searchVal) res = res.filter(activity => {
+            return activity.description.toLowerCase().includes(searchVal)
+            || activity.byUser.fullName.toLowerCase().includes(searchVal)
+        })
         return res;
     }
 
     render() {
         if (!this.props.activityLog) return <h1>Loading...</h1>
         const {isFilterOpen, filterBy, searchVal } = this.state;
-        let [activities, activitiesNotRead] = this.getActivities();
-        const dates = this.getActivityDates(activitiesNotRead, activities)
-        console.log('filtered:', this.applyFilter(activitiesNotRead));
+        let [activities, activitiesNotRead] = this.activities;
+        const dates = this.getActivityDates();
+        const members = this.getActivityMembers();
+        console.log('activities:', activities, activitiesNotRead);
+        activities = this.applyFilter(activities)
+        activitiesNotRead = this.applyFilter(activitiesNotRead)
         return (
             <section className="activities flex column padding-y-15">
 
@@ -97,6 +99,10 @@ export default class Activities extends Component {
                                 <div className="filter-modal flex absolute">
                                     <section className="activity-member-filter">
                                         <h3>Member</h3>
+                                        {members.map((member, idx) => <button
+                                                className={filterBy.member === member ? 'remove-filter-btn' : ''}
+                                                key={idx}
+                                                onClick={() => this.onSetFilter('member', member)}>{member}</button>)}
                                     </section>
                                     <section className="activity-date-filter">
                                         <h3>Date</h3>
