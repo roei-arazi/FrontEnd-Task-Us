146917ab9d790343273725554b9229aeec8dae83
OsherK <osherzasx@gmail.com>
2020-09-26 14:46:29 +0300

    m

--- src/cmps/Activities.jsx
+++ src/cmps/Activities.jsx
@@ -22,6 +22,13 @@ export default class Activities extends Component {
         return [activityLog.filter(activity => activity.isRead), activityLog.filter(activity => !activity.isRead)]
     }
 
+    getInitials(fullName){
+        const [firstName, lastName] = fullName.split(' ');
+        let initials = firstName.charAt(0).toUpperCase();
+        if(lastName) initials += lastName.charAt(0).toUpperCase();
+        return initials;
+    }
+
     handleChange = ({ target }) => {
         this.setState({ searchVal: target.value })
     }
@@ -141,7 +148,8 @@ export default class Activities extends Component {
                                                 {moment(activity.createdAt).format("DD MMM")}
                                             </p>
                                         </div>
-                                        <img src={activity.byUser.imgUrl} alt="" />
+                                        {activity.byUser.imgUrl ? <img src={activity.byUser.imgUrl} alt="" />:
+                                        <div className="member-letter flex align-center justify-center">{this.getInitials(activity.byUser.fullName)}</div>}
                                         <h2>{activity.byUser.fullName}</h2>
                                     </div>
                                     <div className="activity-desc-container flex align-center">
