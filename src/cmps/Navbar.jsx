9ab3cc7a1ca001ac824e8c7a9e641d3cfd8ac84b
Your Name <you@example.com>
2020-09-27 12:50:30 +0300

    m

--- src/cmps/Navbar.jsx
+++ src/cmps/Navbar.jsx
@@ -61,7 +61,7 @@ class _Navbar extends Component {
                         <li className="icon-container"><BsCalendar /></li>
                     </NavLink>
                     <NavLink to={loggedUser ? `/user/${loggedUser._id}` : '/login'}>
-                        <li className="icon-container"><FaRegUser /></li>
+                        <li className="icon-container"> {loggedUser.imgUrl ? <img class="small-profile-img" src={loggedUser.imgUrl} alt="" /> : <FaRegUser />}</li>
                     </NavLink>
                     <li className="icon-container" ><BiLogOut onClick={this.onLogout} /></li>
                 </ul>
